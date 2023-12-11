3dacd61e6f07fa934e04852f174d36476238840e
YunYouJun <me@yunyoujun.cn>
2023-12-11 14:43:36 +0800

    feat: add shim for cjs require by esm

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,6 +1,6 @@
-import type { Options } from 'tsup'
+import { defineConfig } from 'tsup'
 
-export default <Options>{
+export default defineConfig({
   entryPoints: [
     'src/*.ts',
   ],
@@ -8,4 +8,12 @@ export default <Options>{
   format: ['cjs', 'esm'],
   dts: true,
   onSuccess: 'npm run build:fix',
-}
+  banner: {
+    // https://github.com/evanw/esbuild/issues/1921
+    js: `
+    const require = (await import("node:module")).createRequire(import.meta.url);
+    const __filename = (await import("node:url")).fileURLToPath(import.meta.url);
+    const __dirname = (await import("node:path")).dirname(__filename);
+    `,
+  },
+})
