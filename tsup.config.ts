926168e7ee3117dabe954c23a49c1fe18e6494b0
YunYouJun <me@yunyoujun.cn>
2023-12-15 11:40:03 +0800

    fix: add cjs esm adapt

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,23 +1,22 @@
 import { defineConfig } from 'tsup'
 
-export default defineConfig(({ format }) => {
-  return {
-    entryPoints: [
-      'src/*.ts',
-    ],
-    clean: true,
-    format: ['cjs', 'esm'],
-    dts: true,
-    onSuccess: 'npm run build:fix',
-    banner: {
-      // https://github.com/evanw/esbuild/issues/1921
-      js: format === 'esm'
-        ? `
-      const require = (await import("node:module")).createRequire(import.meta.url);
-      const __filename = (await import("node:url")).fileURLToPath(import.meta.url);
-      const __dirname = (await import("node:path")).dirname(__filename);
-      `
-        : '',
-    },
-  }
+export default defineConfig({
+  entryPoints: [
+    'src/*.ts',
+  ],
+  clean: true,
+  format: ['cjs', 'esm'],
+  dts: true,
+  onSuccess: 'npm run build:fix',
+  banner: {
+    // https://github.com/evanw/esbuild/issues/1921
+    js: `
+// adapt for commonjs
+if (typeof require !== 'undefined' && typeof __filename !== 'undefined' && typeof __dirname !== 'undefined') {
+  const require = (await import("node:module")).createRequire(import.meta.url);
+  const __filename = (await import("node:url")).fileURLToPath(import.meta.url);
+  const __dirname = (await import("node:path")).dirname(__filename);
+}
+    `,
+  },
 })
