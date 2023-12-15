26dee11d9dc57aba34885ff2937bc262d89fc504
YunYouJun <me@yunyoujun.cn>
2023-12-15 14:57:54 +0800

    fix: picomatch in rollup/pluginutils for esm vitest

--- tsup.config.ts
+++ tsup.config.ts
@@ -8,15 +8,17 @@ export default defineConfig({
   format: ['cjs', 'esm'],
   dts: true,
   onSuccess: 'npm run build:fix',
-  banner: {
-    // https://github.com/evanw/esbuild/issues/1921
-    js: `
-// adapt for commonjs
-if (typeof require === 'undefined' && typeof __filename === 'undefined' && typeof __dirname === 'undefined') {
-  var require = (await import("node:module")).createRequire(import.meta.url);
-  var __filename = (await import("node:url")).fileURLToPath(import.meta.url);
-  var __dirname = (await import("node:path")).dirname(__filename);
-}
-    `,
-  },
+  // depend by unplugin chokidar
+  external: ['picomatch'],
+  //   banner: {
+  //     // https://github.com/evanw/esbuild/issues/1921
+  //     js: `
+  // // adapt for cjs
+  // if (typeof require !== 'undefined' && typeof __filename !== 'undefined' && typeof __dirname !== 'undefined') {
+  //   const require = (await import("node:module")).createRequire(import.meta.url);
+  //   const __filename = (await import("node:url")).fileURLToPath(import.meta.url);
+  //   const __dirname = (await import("node:path")).dirname(__filename);
+  // }
+  //     `,
+  //   },
 })
